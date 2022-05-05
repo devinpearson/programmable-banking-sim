5e6382480fd2f498be19d2d2290986d03db19efb
Devin <devin@blackhat.co.za>
2022-05-05 21:30:17 +0200

    added additional functions and moved to es6 imports

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const fs = require('fs')
-const dayjs = require('dayjs')
+import 'dotenv/config'
+import express from 'express'
+import cards from './assets/data/cards.json'
+import cors from 'cors'
+import dayjs from 'dayjs'
+import countries from './assets/data/countries.json'
+import currencies from './assets/data/currencies.json'
+import merchants from './assets/data/merchants.json'
+import accounts from './assets/data/accounts.json'
 
 const app = express()
 const port = process.env.PORT || 3000
-const datafile = process.env.DATAFILE || 'data/accounts.json'
+// const datafile = process.env.DATAFILE || 'data/accounts.json'
 
 app.use(cors())
-
 // Configuring body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(express.json())
 
 const accessTokens = {}
-
-const accounts = JSON.parse(fs.readFileSync(datafile, 'utf8'))
 
 app.post('/identity/v2/oauth2/token', (req, res) => {
   const authStr = Buffer.from(req.headers.authorization.split(' ')[1], 'base64').toString()
@@ -93,9 +92,9 @@ app.get('/za/pb/v1/accounts/:accountId/transactions', (req, res) => {
   }
   const accountId = req.params.accountId
 
-  const toDate = req.query.toDate ?? dayjs().format('YYYY-MM-DD') // set to today
-  const fromDate = req.query.fromDate ?? dayjs().subtract(180, 'day').format('YYYY-MM-DD') // set to 180 in the passed
-  const transactionType = req.query.transactionType ?? null
+  const toDate = req.query.toDate || dayjs().format('YYYY-MM-DD') // set to today
+  const fromDate = req.query.fromDate || dayjs().subtract(180, 'day').format('YYYY-MM-DD') // set to 180 in the passed
+  const transactionType = req.query.transactionType || null
 
   for (let i = 0; i < accounts.length; i++) {
     if (accounts[i].accountId === accountId) {
@@ -121,34 +120,102 @@ app.get('/za/pb/v1/accounts/:accountId/transactions', (req, res) => {
   return res.status(404).json() // no account was found
 })
 
+app.get('/za/v1/cards', (req, res) => {
+  if (!isValidToken(req)) {
+    return res.status(401).json()
+  }
+  const returnCards = []
+  for (let i = 0; i < cards.length; i++) {
+    const card = {
+      CardKey: cards[i].CardKey,
+      CardNumber: cards[i].CardNumber,
+      IsProgrammable: cards[i].IsProgrammable,
+      Status: cards[i].Status,
+      CardTypeCode: cards[i].CardTypeCode,
+      AccountNumber: cards[i].AccountNumber,
+      AccountId: cards[i].AccountId
+    }
+    returnCards.push(card)
+  }
+  const data = { cards: returnCards }
+  return formatResponse(data, req, res)
+})
+
+app.get('/za/v1/cards/:cardkey/code', (req, res) => {
+  if (!isValidToken(req)) {
+    return res.status(401).json()
+  }
+  const cardkey = req.params.cardkey
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].CardKey === cardkey) {
+      return formatResponse(cards[i].saved, req, res)
+    }
+  }
+  return res.status(404).json() // no card was found
+})
+
+app.get('/za/v1/cards/:cardkey/publishedcode', (req, res) => {
+  if (!isValidToken(req)) {
+    return res.status(401).json()
+  }
+  const cardkey = req.params.cardkey
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].CardKey === cardkey) {
+      return formatResponse(cards[i].published, req, res)
+    }
+  }
+  return res.status(404).json() // no card was found
+})
+
+app.post('/za/v1/cards/:cardkey/environmentvariables', (req, res) => {
+  if (!isValidToken(req)) {
+    return res.status(401).json()
+  }
+  const cardkey = req.params.cardkey
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].CardKey === cardkey) {
+      cards[i].variables = req.body
+      cards[i].variables.createdAt = dayjs().format('YYYY-MM-DDTHH:mm:ss.SSS[Z]'),
+      cards[i].variables.updatedAt = dayjs().format('YYYY-MM-DDTHH:mm:ss.SSS[Z]'),
+      cards[i].variables.error = null
+      return formatResponse(cards[i].variables, req, res)
+    }
+  }
+  return res.status(404).json() // no card was found
+})
+
+app.get('/za/v1/cards/:cardkey/environmentvariables', (req, res) => {
+  if (!isValidToken(req)) {
+    return res.status(401).json()
+  }
+  const cardkey = req.params.cardkey
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].CardKey === cardkey) {
+      return formatResponse(cards[i].variables, req, res)
+    }
+  }
+  return res.status(404).json() // no card was found
+})
+
 app.get('/za/v1/cards/countries', (req, res) => {
   if (!isValidToken(req)) {
     return res.status(401).json()
   }
-  fs.readFile('data/countries.json', 'utf8', function (err, data) {
-    if (err) throw err
-    res.json(JSON.parse(data))
-  })
+  res.json(countries)
 })
 
 app.get('/za/v1/cards/currencies', (req, res) => {
   if (!isValidToken(req)) {
     return res.status(401).json()
   }
-  fs.readFile('data/currencies.json', 'utf8', function (err, data) {
-    if (err) throw err
-    res.json(JSON.parse(data))
-  })
+  res.json(currencies)
 })
 
 app.get('/za/v1/cards/merchants', (req, res) => {
   if (!isValidToken(req)) {
     return res.status(401).json()
   }
-  fs.readFile('data/merchants.json', 'utf8', function (err, data) {
-    if (err) throw err
-    res.json(JSON.parse(data))
-  })
+  res.json(merchants)
 })
 
 const formatResponse = (data, req, res) => {
